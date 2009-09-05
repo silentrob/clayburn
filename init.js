4413dce0623b73a2adbbeba66241442703397563
Brock Whitten <brock@sintaxi.com>
2009-09-04 18:36:56 -0700

    more repo cleanup

--- init.js
+++ init.js
@@ -1,10 +1,10 @@
 system.use("clayburn.lib.debug");
-system.use("clayburn.lib.smart-router");
-system.use("clayburn.lib.smart-controller");
-system.use("clayburn.lib.smart-model");
-system.use("clayburn.lib.smart-template");
-system.use("clayburn.lib.smart-exceptions");
-system.use("clayburn.lib.smart-mime");
+system.use("clayburn.lib.router");
+system.use("clayburn.lib.controller");
+system.use("clayburn.lib.model");
+system.use("clayburn.lib.template");
+system.use("clayburn.lib.exceptions");
+system.use("clayburn.lib.mime");
 system.use("clayburn.lib.core-ext");
 system.use("clayburn.lib.ejs");
 system.use("clayburn.lib.ejsview")
