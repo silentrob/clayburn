e05b40f1c096b1a89b6258478cb663f17d919edd
Rob Ellis <kazoomer@gmail.com>
2009-09-04 18:09:40 -0700

    repo cleanup

---
+++ init.js
@@ -0,0 +1,10 @@
+system.use("clayburn.lib.debug");
+system.use("clayburn.lib.smart-router");
+system.use("clayburn.lib.smart-controller");
+system.use("clayburn.lib.smart-model");
+system.use("clayburn.lib.smart-template");
+system.use("clayburn.lib.smart-exceptions");
+system.use("clayburn.lib.smart-mime");
+system.use("clayburn.lib.core-ext");
+system.use("clayburn.lib.ejs");
+system.use("clayburn.lib.ejsview")
--- run-smart.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/sh
- 
-HOME=`pwd` /Applications/Smart.app/Contents/Resources/Image/bin/rspd `pwd`
